f827e939936f843b005ce16c7d30cbf62c162c18
michisakthi <sakthivel.subbaian@gmail.com>
2015-05-27 20:51:54 +0200

    Update DAPAs_l1.cpp
    Completed up to local hull calculation.
    Added run time input for number of points.
    Added customized MPI data type to transfer data between processes.(successfully)
    Its only tested in 4 processes.

--- DAPAs_l1.cpp
+++ DAPAs_l1.cpp
@@ -15,27 +15,29 @@
 #include <mpi/mpi.h>
 #include <stddef.h>
 using namespace std;
-
+#define debug 0
 /*
  * 
  */
 // Define Infinite (Using INT_MAX caused overflow problems)
 #define INF 10000
 
-static double gettime(void) {
+static double gettime(void)
+{
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return tv.tv_sec + 1e-6 * tv.tv_usec;
+    return tv.tv_sec + 1e-6*tv.tv_usec;
 }
 
-typedef struct P {
+typedef struct p {
     float x;
     float y;
 } Point;
 
+
 typedef enum {
-    TURN_RIGHT = 1,
-    TURN_LEFT = 2
+	TURN_RIGHT = 1,
+	TURN_LEFT = 2
 } turn_t;
 
 // To find orientation of ordered triplet (p, q, r).
@@ -45,168 +47,164 @@ typedef enum {
 // 2 --> Counterclockwise
 
 int orientation(Point p, Point q, Point r) {
-    float val = ((q.x - p.x) * (r.y - p.y)) - ((r.x - p.x) * (q.y - p.y));
+	float val = ((q.x - p.x) * (r.y - p.y)) - ((r.x - p.x) * (q.y - p.y));
 
-    if (val <= 0) // right turn  or collinear
-        return 1;
-    else //left turn
-        return 2;
+	if (val <= 0)   // right turn  or collinear
+		return 1;
+	else         //left turn
+		return 2;
 }
 /*
 Point getPoint(float *points, int k)
 {
-return Point
+	return Point
 }
- */
+*/
 // Prints convex hull of a set of n points.
-
 void convexHull(Point points[], int n, int *Hull) {
-    // There must be at least 3 points (6 floats)
-    if (n < 3)
-        return;
+	// There must be at least 3 points (6 floats)
+	if (n < 3)
+		return;
 
-    // Initialize Result
-    //	int *Hull = new int[n];
-    //int next[n];
-    for (int k = 0; k < n; k++)
-        Hull[k] = -1;
+	// Initialize Result
+//	int *Hull = new int[n];
+	//int next[n];
+	for (int k = 0; k < n; k++)
+		Hull[k] = -1;
 
-    // Find the leftmost point
-    int l = 0;
-    for (int k = 0; k < n; k++) {
-        if (points[k].x < points[l].x)
-            l = k;
-    }
-    // Start from leftmost point, keep moving counterclockwise
-    // until reach the start point again
-    int q = l;
-    int i = 0;
-    do {
-        Hull[i] = q;
-        i = i + 1;
-        int p = q;
-        q = 0; // index of the first point in the points array
-        // Search for a point 'q' such that orientation(p, i, q) is
-        // counterclockwise for all points 'i'
-        if (p == q) // if P and Q become same point move the Q to the next point in the array(Assumption 1)
-        {
-            q = q + 1;
-        }
-        for (int j = 1; j < n; j++) {
-            if (j == p) // If P and R(j) become same point then skip that point.
-                continue;
+	// Find the leftmost point
+	int l = 0;
+	for (int k = 0; k < n; k++){
+		if (points[k].x < points[l].x)
+			l = k;
+	}
+	// Start from leftmost point, keep moving counterclockwise
+	// until reach the start point again
+	int q = l;
+	int i = 0;
+	do {
+		Hull[i] = q;
+		i = i + 1;
+		int p = q;
+		q = 0; // index of the first point in the points array
+		// Search for a point 'q' such that orientation(p, i, q) is
+		// counterclockwise for all points 'i'
+		
+		if (p == q)	// if P and Q become same point move the Q to the next point in the array(Assumption 1)
+		{
+			q = q + 1;
+		}
+		for (int j = 1; j < n; j++){
+			if (j == p)		// If P and R(j) become same point then skip that point.
+				continue;
 
-            if (orientation(points[p], points[q], points[j]) == TURN_LEFT) {
-                q = j;
-            }
-        }
-    } while (q != Hull[0]);
+			if (orientation(points[p], points[q], points[j]) == TURN_LEFT)
+			{
+				q = j;
+			}
+		}
+	} while (q != Hull[0]);
 
-    /*	// Print Result
-    for (int i = 0; i < n; i++) {
-    if (Hull[i] != -1)
-    cout << "(" << points[Hull[i]].x << ", " << points[Hull[i]].y << ")\n";
-    }
-     */
+/*	// Print Result
+	for (int i = 0; i < n; i++) {
+		if (Hull[i] != -1)
+			cout << "(" << points[Hull[i]].x << ", " << points[Hull[i]].y << ")\n";
+	}
+*/
 }
 // Driver program to test above functions
 
 float rand_coord(float limit) {
-    float safe_limit = limit == 0 ? 1l : limit;
-    return (fmod((float) rand(), (float) safe_limit));
+  float safe_limit = limit == 0 ? 1l : limit;
+  return (fmod((float) rand() , (float) safe_limit)); 
 }
 
 Point random_point(float limit) {
-
-    return (Point) {
-        rand_coord(limit), rand_coord(limit)
-    };
+  return (Point) { rand_coord(limit), rand_coord(limit) };
 }
 
 int main(int argc, char *argv[]) {
 
-    int m = 100;
+	int myid, numprocs, i = 0, m = 40;
 
-    // generate random inputs points.
-    m = 80;
-    //float *points1 = new float[m];
-    int q = 10;
-    //float *points2 = new float[q];
+ if (argc >= 2) {
+	  
+  m = atoi(argv[1]);
+    if (m < 40)
+      m = 40;
+  }else{
+		printf("you forgot total number of points!");
+		return 0;
+	}
 
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 
-    int myid, numprocs, i = 0;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+	/* create a type for struct car */
+	const int nitems = 2;
+	int blocklengths[2] = {1, 1};
+	MPI_Datatype types[2] = {MPI_FLOAT, MPI_FLOAT};
+	MPI_Datatype mpi_point_type;
+	MPI_Aint offsets[2];
+	offsets[0] = offsetof(Point, x);
+	offsets[1] = offsetof(Point, y);
+	MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_point_type);
+	MPI_Type_commit(&mpi_point_type);
 
+	Point *send = NULL;
 
-    /* create a type for struct Point */
-    const int nitems = 2;
-    int blocklengths[2] = {1, 1};
-    MPI_Datatype types[2] = {MPI_FLOAT, MPI_FLOAT};
-    MPI_Datatype mpi_point_type;
-    MPI_Aint offsets[2];
-    offsets[0] = offsetof(Point, x);
-    offsets[0] = offsetof(Point, y);
-    MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_point_type);
-    MPI_Type_commit(&mpi_point_type);
-
-    Point *points1 = new Point[40];
-    Point *points2 = new Point[10];
-
-
-    printf("Who am i = %d\n", myid);
-
-    if (myid == 0) {
-        for (i = 0; i < m; ++i) {
-            points1[i] = rand_coord(100);
-        }
-
-        /* Print out own portion of the scattered array */
-        printf("Root has elements");
-        for (i = 0; i < m; i++) {
-            printf(" %.0f", points1[i]);
-            if (i % 19 == 0) printf("\n");
-        }
-        printf("\n");
-
-    }
+	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
 
+//	double t1 = gettime();
 
-    MPI_Scatter(points1, q, MPI_FLOAT, points2, q, mpi_point_type, 0, MPI_COMM_WORLD);
+	if (myid == 0) {
+//			int 
+			send=new Point[m];
+			for(i = 0; i<m;++i)
+			{
+				send[i] = random_point(100);
+			}
+		if(debug){
+			printf("Root has elements");
+			for (i=0; i<m; i++) {
+				printf(" (%.0f, %.0f)", send[i].x, send[i].y);
+				if(i%9 == 0)	printf("\n");
+			}
+			printf("\n");
+		}
+	}
+	int q = 10;
+	q = m/numprocs;
+	Point *recv=new Point[q];
 
-    int pointsLength = 10;
-    Point *myPoints = new Point[pointsLength];
-    int *Hull = new int[pointsLength];
-    for (i = 0; i < pointsLength; i++) {
-        myPoints[i].x = points2[i * 2];
-        myPoints[i].y = points2[i * 2 + 1];
-    }
+	MPI_Scatter(send, q, mpi_point_type, recv, q, mpi_point_type, 0,MPI_COMM_WORLD);
 
-    printf("Process %d has elements:", myid);
-    for (i = 0; i < pointsLength; i++) {
-        //      printf(" %.0f", points2[i]);
-        cout << "(" << points2[i].x << ", " << points2[i].y << ") ";
-    }
-    printf("\n");
+	if(debug){
+		printf("Process %d has elements:", myid);
+		for (i=0; i<q; i++) {
+			printf(" (%.0f, %.0f)", recv[i].x, recv[i].y);
+		}
+		printf("\n");
+	}
 
-    double t1 = gettime();
-    convexHull(points2, pointsLength, Hull);
-    double t2 = gettime();
-    double t3 = t2 - t1;
+	int *Hull = new int[q];
+	double t1 = gettime();
+	convexHull(recv, q, Hull);
+	double t2 = gettime();
+	double t3 = t2 - t1;
 
-    // Print Result
-    printf("Process %d found Hull( elements):", myid);
-    for (int i = 0; i < pointsLength; i++) {
-        if (Hull[i] != -1)
-            cout << "(" << points2[Hull[i]].x << ", " << points2[Hull[i]].y << ") ";
-    }
-    printf("\n");
-    printf("\n Run Time = %.6lf\n", t3);
-
-
-
-    MPI_Finalize();
-    return 0;
+	if(debug){
+		// Print Result
+		printf("Process %d found Hull( elements):", myid);
+		for (int i = 0; i < q; i++) {
+				if (Hull[i] != -1)
+				    cout << "(" << recv[Hull[i]].x << ", " << recv[Hull[i]].y << ") ";
+		}      
+		printf("\n");
+	}
+	printf("\n Run Time = %.6lf\n", t3);
+	MPI_Type_free(&mpi_point_type);
+	MPI_Finalize();
+	return 0;
 }
